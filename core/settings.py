6938900fe93fd4519d4aa897e57f329ed4713651
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-05-11 22:17:17 -0500

    se crearon tailwind theme

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "blog",
+    "tailwind",
+    "theme",
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
