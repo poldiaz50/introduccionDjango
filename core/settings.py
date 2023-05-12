0775ed81af805aa743e2af6a961fd923548f37e8
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-05-12 00:35:22 -0500

    se instalo tailwind

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "blog",
     "tailwind",
     "theme",
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -42,6 +43,8 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView.as_view(), name="home"),
     path("blog/", include("blog.urls", namespace="blog")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
